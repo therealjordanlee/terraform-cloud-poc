f4541680cdbfc6a0a127caa32631ba47cc40fe6e
Jordan Lee <jordan@jordanlee.net>
2022-10-18 11:44:57 +1100

    Add dummy tf files

---
+++ backend.tf
@@ -0,0 +1,14 @@
+# The block below configures Terraform to use the 'remote' backend with Terraform Cloud.
+# For more information, see https://www.terraform.io/docs/backends/types/remote.html
+terraform {
+  cloud {
+    organization = "jordan_corp"
+
+    ## Required for Terraform Enterprise; Defaults to app.terraform.io for Terraform Cloud
+    hostname = "app.terraform.io"
+    
+    workspaces {
+      name = "temenos_cloud"
+    }
+  }
+}
