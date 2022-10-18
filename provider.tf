0c9ccda35126cebeb41d4c6ff212e775a954d7e8
Jordan Lee <jordan@jordanlee.net>
2022-10-18 12:39:26 +1100

    Split variables into separate file

--- provider.tf
+++ provider.tf
@@ -1,25 +1,3 @@
-# The following variable is used to configure the provider's authentication
-# token. You don't need to provide a token on the command line to apply changes,
-# though: using the remote backend, Terraform will execute remotely in Terraform
-# Cloud where your token is already securely stored in your workspace!
-
-variable "subscription_id" {
-  type = string
-}
-
-variable "client_id" {
-  type = string
-}
-
-variable "client_secret" {
-  type = string
-  sensitive = true
-}
-
-variable "tenant_id" {
-  type = string
-}
-
 provider "azurerm" {
   features {}
   subscription_id = var.subscription_id
